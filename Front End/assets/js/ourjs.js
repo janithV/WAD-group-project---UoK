620178bd5fe7357531d9bc85e6b750b4fe453fbf
shaniAbeywick <shaniabeywickrama294@gmail.com>
2020-07-19 19:12:52 +0530

    sign up page added

---
+++ Front End/assets/js/ourjs.js
@@ -0,0 +1,13 @@
+
+!(function($) {
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  })(jQuery);
