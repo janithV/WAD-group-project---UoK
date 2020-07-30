a4e38a1df85ade9133a0e6f07042cd67daf876bd
janithV <55439779+janithV@users.noreply.github.com>
2020-07-30 09:47:14 +0530

    event.js file with edited responses

--- Backend/api/routes/events.js
+++ Backend/api/routes/events.js
@@ -2,40 +2,36 @@ const conn= require('../../connection')
 const express = require('express');
 const router=express.Router();
 
-//getting all events
+//localhost:3000/events
 router.get('/',(req,res,next)=>{
     conn.query("SELECT * from event", (err,rows,fields)=>{
         if(!err){
             console.log(rows);
+            res.status(200).send(rows);
         }
         else{
             console.log(err);
         }
     })
-
-    res.status(200).json({
-        message: 'handling GET to /events'
-    });
 });
-
+//localhost:3000/events/(eventId)
 router.get('/:eventId',(req,res,next)=>{
     const id =req.params.eventId;
 
     conn.query("SELECT * from event where eventid =?",[id], (err,rows,fields)=>{
         if(!err){
             console.log(rows);
+            res.status(200).send(rows);
         }
         else{
             console.log(err);
         }
     })
-    res.status(200).json({
-        message: 'you clicked on this event',
-        id :id
-    });
+
 });
 
-router.post('/addEvent',(req,res,next)=>{
+//localhost:3000/events/addEvent
+router.post('/addEvent',(req,res,next)=>{ 
     const event ={
         name: req.body.name,
         date: req.body.date,
@@ -50,48 +46,51 @@ router.post('/addEvent',(req,res,next)=>{
     conn.query("INSERT INTO event (name,date,venue,description,starttime,duration) VALUES ('"+event.name+"','"+event.date+"','"+event.venue+"','"+event.description+"','"+event.startTime+"','"+event.duration+"')", (err,result)=>{
         if(!err){
             console.log(result.insertId);
+            res.status(201).json({
+                message: 'Event added',
+                id: result.insertId
+            });
         }
         else{
             console.log(err);
         }
     })
-    res.status(200).json({
-        message: 'you clicked on add event'
-    });
+    
 });
 
+//localhost:3000/events/deleteEvent/(eventId)
 router.post('/deleteEvent/:eventId',(req,res,next)=>{
     const id =req.params.eventId;
 
     conn.query("DELETE FROM event WHERE eventid =?",[id], (err,result)=>{
         if(!err){
             console.log(result.affectedRows);
+            res.status(201).json({
+                message: 'Event deleted',
+                id :id
+            });
         }
         else{
             console.log(err);
         }
     })
-    res.status(200).json({
-        message: 'you clicked on this event',
-        id :id
-    });
+    
 });
 
-router.post('/myEvents/:custId',(req,res,next)=>{
-    const id =req.params.custId;
+//localhost:3000/events/myEvents/(custId)
+router.get('/myEvents/:custId',(req,res,next)=>{
+    const id =req.params.custId; 
 
     conn.query("SELECT * FROM event WHERE customerid =?",[id], (err,rows,fields)=>{
         if(!err){
             console.log(rows);
+            res.status(200).send(rows);
         }
         else{
             console.log(err);
         }
     })
-    res.status(200).json({
-        message: 'you clicked on this event',
-        id :id
-    });
+    
 });
 
 module.exports=router;
