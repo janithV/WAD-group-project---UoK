03dd99ec52fa52be040ecd224431fd9b915594e0
janithV <55439779+janithV@users.noreply.github.com>
2020-08-01 18:49:48 +0530

    customer router added

--- Backend/api/routes/customer.js
+++ Backend/api/routes/customer.js
@@ -29,3 +29,32 @@ router.get('/:custID',(req,res,next)=>{
     });
 });
 
+router.post('/addCustomer',(req,res,next)=>{ 
+    const Customer ={
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.date,
+        contactno: req.body.venue,
+        password: req.body.description,
+        companyName: req.body.startTime,
+        companyAddress: req.body.duration,
+        position: req.body.customerId
+        
+    }
+
+    conn.query("INSERT INTO Customer (firstname,lastname,venue,email,contactno,password,companyname,companyaddress,position)" +
+    "VALUES ('"+Customer.fname+"','"+Customer.lname+"','"+Customer.email+"','"+Customer.contactno+"','"+Customer.password+"','"+Customer.companyName+"','"+Customer.companyAddress+"','"+Customer.position+"')", (err,result)=>{
+        if(!err){
+            console.log(result.insertId);
+            res.status(201).json({
+                message: 'Customer added',
+                id: result.insertId
+            });
+        }
+        else{
+            console.log(err);
+        }
+    })
+    
+});
+
