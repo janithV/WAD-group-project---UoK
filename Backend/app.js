97bf294cd4845fbb81408a8ca4851a7e01dcb321
janithV <55439779+janithV@users.noreply.github.com>
2020-08-08 19:37:03 +0530

    commented

--- Backend/app.js
+++ Backend/app.js
@@ -33,19 +33,19 @@ app.use(fileupload({
   
 
 //error handling
-app.use((req,res,next)=>{
-    const error = new Error('Not Found');
-    error.status=404;
-    next(error);
-});
+// app.use((req,res,next)=>{
+//     const error = new Error('Not Found');
+//     error.status=404;
+//     next(error);
+// });
 
-app.use((error,req,res,next)=>{
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message :error.message
-        }
-    })
-});
+// app.use((error,req,res,next)=>{
+//     res.status(error.status || 500);
+//     res.json({
+//         error:{
+//             message :error.message
+//         }
+//     })
+// });
 
 module.exports=app;
